811e18aefbdaa61e4a90c67578b62943864c70a7
Ben Bos <ben@dotlabs.nl>
2015-01-27 13:55:11 +0100

    Removed Grunt loadNpmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,20 +23,4 @@ module.exports = function(grunt) {
         "clean:temp"
     ]);
 
-    grunt.loadNpmTasks("grunt-contrib-clean");
-    grunt.loadNpmTasks("grunt-contrib-copy");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks("grunt-usemin");
-    grunt.loadNpmTasks("grunt-contrib-compass");
-    grunt.loadNpmTasks("grunt-filerev");
-    grunt.loadNpmTasks("grunt-contrib-requirejs");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-jscs");
-    // grunt.loadNpmTasks("grunt-contrib-rename");
-    grunt.loadNpmTasks("grunt-strip");
-    // grunt.loadNpmTasks("grunt-string-replace");
-    // grunt.loadNpmTasks("grunt-notify");
 };
